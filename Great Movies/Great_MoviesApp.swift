cbfd18c4cb8bea0d2e4c0f3f1d5112f9c98044dc
Juliano <alvarenga.vix@gmail.com>
2024-03-08 13:12:04 -0300

    Initial Commit

---
+++ Great Movies/Great_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Great_MoviesApp.swift
+//  Great Movies
+//
+//  Created by Juliano Alvarenga on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Great_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
